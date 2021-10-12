a46c01eca8e9165adbd6288ed643e7b6ad25f4d8
Ayrat Mukhametshin <airatmm@gmail.com>
2021-10-12 22:35:03 +0300

    fix/after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 const editButton = document.querySelector('.profile__button_action_edit');
+
 const formButton = document.querySelector('.popup__button');
 const form = document.querySelector('.popup__form');
+
 const titleProfile = document.querySelector('.profile__title');
-const titleField = document.querySelector('.popup__input_title');
-const discriptionField = document.querySelector('.popup__input_discription');
+const titleField = document.querySelector('.popup__input_type_title');
+
+const discriptionField = document.querySelector('.popup__input_type_discription');
 const discriptionText = document.querySelector('.profile__discription');
 
 //обработчик открытия попапа
 function openPopup() {
-    popup.classList.add('popup__open');
+    popup.classList.add('popup_opened');
     titleField.value = titleProfile.textContent;
     discriptionField.value = discriptionText.textContent;
 }
 
 //обработчик закрытия попапа
 function closePopup() {
-    popup.classList.remove('popup__open');
+    popup.classList.remove('popup_opened');
 }
-
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
 
 function submitForm(event) {
     event.preventDefault();
@@ -31,5 +31,8 @@ function submitForm(event) {
 
     closePopup();
 }
+
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
 
 form.addEventListener('submit', submitForm);

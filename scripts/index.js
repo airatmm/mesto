c0ef40173e74c3300f5cd24e711e2f13d26d435c
Ayrat Mukhametshin <airatmm@gmail.com>
2021-12-10 09:17:23 +0300

    feature: Начало спринта 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,51 @@
-const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__close');
-const editButton = document.querySelector('.profile__button_action_edit');
+const popup = document.querySelector('.popup'); //возвращаем элемент popup (div popup)
+const popupClose = document.querySelector('.popup__close'); //возвращаем элемент popup__close (кнопка закрытия попапа) (div popup)
+const editButton = document.querySelector('.profile__button_action_edit'); //возвращаем элемент profile__button_action_edit (копка редактирования профиля) из секции profile
 
-const formButton = document.querySelector('.popup__button');
-const form = document.querySelector('.popup__form');
+const formButton = document.querySelector('.popup__button'); //возвращаем элемент popup__button (кнопка сохраниения) (div popup)
+const form = document.querySelector('.popup__form'); //возвращаем элемент popup__form (форма попапа. имя / о себе) (div popup)
 
-const titleProfile = document.querySelector('.profile__title');
-const titleField = document.querySelector('.popup__input_type_title');
+const titleProfile = document.querySelector('.profile__title'); //возвращаем элемент profile__title из секции profile (заголовок профиля (имя))
+const titleField = document.querySelector('.popup__input_type_title'); //возвращаем элемент popup__input_type_title (div popup)  (заголовок профиля (имя) в попапе)
 
-const discriptionField = document.querySelector('.popup__input_type_discription');
-const discriptionText = document.querySelector('.profile__discription');
+const discriptionField = document.querySelector('.popup__input_type_discription'); //возвращаем элемент popup__input_type_discription (div popup) (описание профиля (о себе) в попапе)
+const discriptionText = document.querySelector('.profile__discription'); //возвращаем элемент profile__discription из секции profile (описание профиля (о себе))
+
+// массив карточек
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+
+
+
+
+
+
 
 //обработчик открытия попапа
 function openPopup() {
@@ -22,6 +58,7 @@ function openPopup() {
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
+
 
 function submitForm(event) {
     event.preventDefault();

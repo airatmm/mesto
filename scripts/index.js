74eea784f451cbd77ffe92ffa3f4d2c5d529feb4
Ayrat Mukhametshin <airatmm@gmail.com>
2021-12-14 19:01:59 +0300

    Редактирование профиля работает как и раньше

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
 /* для попапа редактирования профиля*/
 const popupEdit = document.querySelector('.popup_type_edit'); //возвращаем попап редактирования профиля (popup_type_edit) 
 const popupCloseEdit = document.querySelector('.popup__close_edit'); //возвращаем элемент popup__close_edit (кнопка закрытия попапа редактирования)
-console.log(popupCloseEdit);
 const editButton = document.querySelector('.profile__button_action_edit'); //возвращаем элемент profile__button_action_edit (копка редактирования профиля) из секции profile
-
-const formButton = document.querySelector('.popup__button'); //возвращаем элемент popup__button (кнопка сохраниения) (div popup)
-const form = document.querySelector('.popup__form'); //возвращаем элемент popup__form (форма попапа. имя / о себе) (div popup)
+console.log(editButton);
+const formEditSaveButton = document.querySelector('.popup__button_edit_save'); //возвращаем элемент popup__button_edit_save (кнопка сохраниения)
+const formEdit = document.querySelector('.popup__form_edit'); //возвращаем элемент popup__form_edit (форма попапа. имя / о себе) (div popup)
 
 const titleProfile = document.querySelector('.profile__title'); //возвращаем элемент profile__title из секции profile (заголовок профиля (имя))
 const titleField = document.querySelector('.popup__input_type_title'); //возвращаем элемент popup__input_type_title (div popup)  (заголовок профиля (имя) в попапе)
@@ -14,11 +13,14 @@ const discriptionField = document.querySelector('.popup__input_type_discription'
 const discriptionText = document.querySelector('.profile__discription'); //возвращаем элемент profile__discription из секции profile (описание профиля (о себе))
 
 /* для попапа добавления карточек*/
-const addCardButton = document.querySelector('profile__button_action_add'); //кнопка добавления карточки(+)
 const popupAddCard = document.querySelector('popup_type_card'); // попап добавления карточки
+const closeCardButton = document.querySelector('popup__close_card'); //кнопка закрытия попапа добавления карточки
+const addCardButton = document.querySelector('profile__button_action_add'); //кнопка добавления карточки(+)
+console.log(addCardButton);
+
 const inputCardName = document.querySelector('popup__input_card_name'); // инпут названия   
 const inputCardUrl = document.querySelector('popup__input_card_url'); // инпут ссылки / изображения
-const closeCardButton = document.querySelector('popup__close_card'); //кнопка закрытия попапа добавления карточки
+
 const addCardForm = document.querySelector('popup__form_card');
 const addCardSaveButton = document.querySelector('popup__button_card_save'); //кнопка создать новую карточку
 
@@ -109,4 +111,4 @@ function submitForm(event) {
 editButton.addEventListener('click', openPopupEdit);
 popupCloseEdit.addEventListener('click', closePopupEdit);
 
-form.addEventListener('submit', submitForm);
+formEdit.addEventListener('submit', submitForm);

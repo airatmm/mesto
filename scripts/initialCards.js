2437f36f09136d6402112def438964a776570c36
Ayrat Mukhametshin <airatmm@gmail.com>
2022-01-14 17:49:13 +0300

    After review 1.0

---
+++ scripts/initialCards.js
@@ -0,0 +1,29 @@
+// массив карточек
+const initialCards = [
+    {
+        name: 'Retro Car',
+        link: 'https://cdn.pixabay.com/photo/2015/05/28/23/12/auto-788747_960_720.jpg'
+    },
+    {
+        name: 'Телескоп',
+        link: 'https://cdn.pixabay.com/photo/2017/03/08/20/12/viewing-machine-2127704_960_720.jpg'
+    },
+    {
+        name: 'Лондон',
+        link: 'https://cdn.pixabay.com/photo/2014/11/13/23/34/palace-530055_960_720.jpg'
+    },
+    {
+        name: 'Печеньки',
+        link: 'https://cdn.pixabay.com/photo/2019/06/01/21/02/cookie-4245030_960_720.jpg'
+    },
+    {
+        name: 'Рождество',
+        link: 'https://cdn.pixabay.com/photo/2015/02/25/07/39/church-648430_960_720.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+export { initialCards };

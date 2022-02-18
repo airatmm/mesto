90b5201d7a3d246236e1810d49fd5e338e69f13b
Ayrat Mukhametshin <airatmm@gmail.com>
2022-02-18 16:16:32 +0300

    oop/prototype-9sprint

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,6 @@ import {
 import {Card} from "../components/Card.js";
 import {FormValidator} from "../components/FormValidator.js";
 
-import {Popup} from '../components/Popup.js';
 import {Section} from '../components/Section.js';
 import {PopupWithImage} from '../components/PopupWithImage.js';
 import {PopupWithForm} from '../components/PopupWithForm.js';
